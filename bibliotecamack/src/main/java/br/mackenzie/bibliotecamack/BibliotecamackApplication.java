86e5dc2fc97579277dd79143578d473c7842a4d5
carolbegiato <163134994+carolbegiato@users.noreply.github.com>
2026-04-29 11:34:31 +0000

    Craição do Projeto Inicial

---
+++ bibliotecamack/src/main/java/br/mackenzie/bibliotecamack/BibliotecamackApplication.java
@@ -0,0 +1,13 @@
+package br.mackenzie.bibliotecamack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecamackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecamackApplication.class, args);
+	}
+
+}
---
+++ bibliotecamack/src/test/java/br/mackenzie/bibliotecamack/BibliotecamackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.mackenzie.bibliotecamack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecamackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
